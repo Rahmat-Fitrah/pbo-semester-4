616d5359a6cd9b21212170797c7f1678fb8b1ca7
Rahmat-fitrah <rahamtfitrah125@gmail.com>
2026-03-04 18:20:11 +0700

    first commit

---
+++ live coding/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[]args) {
+        Mobil mobilSaya = new Mobil("GTR");
+
+        mobilSaya.gas();
+        mobilSaya.Nyalakan();
+        mobilSaya.gas();
+        mobilSaya.gas();
+        mobilSaya.rem();
+        mobilSaya.matikan();
+    }
+}
+
