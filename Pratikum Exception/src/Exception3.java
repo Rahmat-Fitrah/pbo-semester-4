214c8effeeb6fbded8f4f74038a9f29a346d5ad7
Rahmat-Fitrah <rahmatfitrah125@gmail.com>
2026-05-28 10:18:34 +0700

    Add files via upload

---
+++ Pratikum Exception/src/Exception3.java
@@ -0,0 +1,18 @@
+// Percobaan 3
+public class Exception3 {
+    public static void main(String[] args) {
+        int bil=10;
+        try
+        {
+            System.out.println(bil/0);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Terjadi Aritmatika error");
+        }
+        catch(java.lang.Exception e)
+        {
+            System.out.println("Ini menghandle error yang terjadi");
+        }
+    }
+}
